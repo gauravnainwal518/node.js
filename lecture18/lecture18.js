74dbc25b4a8cc63691d4c594155f8495d7446cc5
gauravnainwal229 <gauravnainwal229@gmail.com>
2024-08-19 15:06:34 +0530

    updated work

---
+++ lecture18/lecture18.js
@@ -0,0 +1,14 @@
+//express js 
+
+const express = require('express');
+const app = express();
+
+app.get('',(req,resp)=>{
+  resp.send('hello this is home page');
+});
+
+app.get('/about',(req,resp)=>{
+  resp.send('hello this is about page');
+});
+
+app.listen(5000)
